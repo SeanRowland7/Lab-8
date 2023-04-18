1e5a0ec90862200edadc85b106aac79b4fd39aec
SeanRowland7 <seanwrowland@gmail.com>
2023-04-18 14:46:38 -0400

    Resolved Merge Conflict

--- src/preprocessor/delegates/ImplicitPointPreprocessor.java
+++ src/preprocessor/delegates/ImplicitPointPreprocessor.java
@@ -30,47 +30,6 @@ public class ImplicitPointPreprocessor
 	 * if there is an intersection between lines -> add it to the list
 	 *    
 	 */
-	
-	/**
-	public static Set<Point> compute(PointDatabase givenPoints, List<Segment> givenSegments)
-	{
-		Set<Point> implicitPoints = new LinkedHashSet<Point>();
-
-		// A PointDatabase containing both given and implicit points to prevent duplicates and facilitate naming.
-		PointDatabase allPoints = new PointDatabase(new ArrayList<Point>(givenPoints.getPoints()));
-		
-		// Compare each segment to all other segments
-		for(int seg1Index = 0; seg1Index < givenSegments.size() - 1; seg1Index++)
-		{
-			for(int seg2Index = seg1Index + 1; seg2Index < givenSegments.size(); seg2Index++)
-			{
-				// Get the segments from their respective indices.
-				Segment seg1 = givenSegments.get(seg1Index);
-				Segment seg2 = givenSegments.get(seg2Index);
-
-				Point intersectionPoint = seg1.segmentIntersection(seg2);
-
-				// If a new intersection point exists then put it in the database.
-				
-				//if valid, put
-				if(intersectionPoint != null && allPoints.getPoint(intersectionPoint) == null) 
-				{
-					// First, add it to the database of points to give it a generated name.
-					allPoints.put(intersectionPoint.getX(), intersectionPoint.getY());
-					
-					// Add the database point to the set of implicit points.
-					implicitPoints.add(allPoints.getPoint(intersectionPoint));
-				}
-			}
-		}
-		// Update the original PointDatabase to include all implicit points.
-		// does nothing
-		givenPoints = allPoints;
-		
-		return implicitPoints;
-		
-		*/
-	
 	public static Set<Point> compute(PointDatabase givenPoints, List<Segment> givenSegments)
 	{
 		Set<Point> implicitPoints = new LinkedHashSet<Point>();
